8317f6ce1d420f85f6632863ecd851ea73d662b5
Daniel Sánchez Gallego <dsanchez@altair.com>
2023-01-10 12:45:10 +0100

    test: add tests for url and path building

--- tests/test_api.py
+++ tests/test_api.py
@@ -28,3 +28,31 @@ class TestAPI(TestCase):
         os.environ.pop("SWX_API_URL")
         with pytest.raises(ValueError):
             API()
+
+
+class TestAPIObject(TestCase):
+    def test_build_url(self):
+        api = API(host="test-api.swx.altairone.com")
+        prop = api.categories("cat01").things("thing01").properties("temperature")
+
+        assert prop.build_url() == "https://test-api.swx.altairone.com" \
+                                   "/categories/cat01/things/thing01/properties/temperature"
+
+    def test_build_path(self):
+        api = API(host="test-api.swx.altairone.com")
+
+        cat = api.categories("cat01")
+        thing1 = cat.things("thing01")
+        thing2 = cat.things("thing02")
+        prop_thing1 = thing1.properties("temperature")
+        prop_thing2 = thing2.properties("humidity")
+
+        assert cat.build_path() == "/categories/cat01"
+        assert thing1.build_path() == "/categories/cat01/things/thing01"
+        assert thing2.build_path() == "/categories/cat01/things/thing02"
+        assert prop_thing1.build_path() == "/categories/cat01/things/thing01/properties/temperature"
+        assert prop_thing2.build_path() == "/categories/cat01/things/thing02/properties/humidity"
+        assert api.things("thing01").build_path() == "/things/thing01"
+        assert api.categories().build_path() == "/categories"
+        assert api.things().build_path() == "/things"
+        assert thing1.properties().build_path() == "/categories/cat01/things/thing01/properties"
