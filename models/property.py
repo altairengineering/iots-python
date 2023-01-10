6c296451b516225278111be8da56978f2b12fd09
Daniel Sánchez Gallego <dsanchez@altair.com>
2023-01-10 14:06:41 +0100

    feat: add PropertiesResp model

---
+++ models/property.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+from models.common import FromDict
+
+
+class PropertiesResp(dict):
+    """
+    The model for the responses from the Properties API.
+    """
+    pass
